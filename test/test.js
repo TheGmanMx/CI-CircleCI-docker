d9cd3ac25f77406966195f8f0718119cb87e4f5f
Unknown <gallardolopezhernan@gmail.com>
2018-01-01 15:16:09 -0800

    Fix to account for mocha 4.0 behaviour change

--- test/test.js
+++ test/test.js
@@ -2,8 +2,7 @@
 'use strict';
 
 var request = require("supertest"),
-    app = require("../app").getApp,
-    assert = require('assert');
+    app = require("../app").getApp;
 
 describe('GET /', function(){
   it('expects HTTP response 200', function(done){
